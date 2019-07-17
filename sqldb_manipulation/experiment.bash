07fc2e51df9b8a710b4f8a21c9012a595330c4d4
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-07-16 21:17:06 -0300

    Fix experiment

--- sqldb_manipulation/experiment.bash
+++ sqldb_manipulation/experiment.bash
@@ -1,8 +1,8 @@
-python3 ./prog;
-python3 ./prog;
-python3 ./prog;
+python3 ./prog.py;
+python3 ./prog.py;
+python3 ./prog.py;
 
 for i in $(seq 10000); do
 	TIMEFORMAT="%E";
-	time { python3 ./prog 1>&2; };
+	time { python3 ./prog.py; };
 done 2> output.txt;
