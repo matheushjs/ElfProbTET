e19e103f6cc3ee48fb24774e39552c74075c4bb8
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-09-03 23:26:47 -0300

    Using the original generalized gamma worked better

--- codes-R/gengamma.r
+++ codes-R/gengamma.r
@@ -3,8 +3,8 @@ require(flexsurv)
 gengamma.infer = function(samples){
 	# The likelihood function
 	likelihood = function(params){
-		# mu > 0, sigma > 0, Q real
-		allLogs = dgengamma(samples, mu=params[1], sigma=params[2], Q=params[3], log=TRUE)
+		# shape > 0, scale > 0, k > 0
+		allLogs = dgengamma.orig(samples, shape=params[1], scale=params[2], k=params[3], log=TRUE)
 
 		# Set all NA and Inf to 0
 		allLogs[!is.finite(allLogs)] = 0
@@ -14,20 +14,17 @@ gengamma.infer = function(samples){
 		return(theSum)
 	}
 
-	lower = c(0, 0, -Inf)
-	upper = Inf
-
 	retval = NULL
 
-	for(mu in c(0.5, 2, 5, 10))
-	for(sigma in c(0.5, 2, 5, 10))
-	for(Q in c(-10, -5, -2, -0.5, 0.5, 2, 5, 10)){
-		params = c(mu, sigma, Q)
+	for(shape in c(0.5, 2, 5, 10))
+	for(scale in c(0.5, 2, 5, 10))
+	for(k in c(0.5, 2, 5, 10)){
+		params = c(shape, scale, k)
 		# print(params)
 		
 		cat("Optimizing with initial params:", params, "\n")
-		result = optim(params, likelihood, lower=lower, upper=upper)
-		result = optim(result$par, likelihood, lower=lower, upper=upper)
+		result = optim(params, likelihood)
+		result = optim(result$par, likelihood)
 		params = result$par
 		val = result$value
 		cat("Got params:", params, "\n")
@@ -36,7 +33,7 @@ gengamma.infer = function(samples){
 	}
 
 	retval = as.data.frame(retval)
-	colnames(retval) = c("mu", "sigma", "Q", "value")
+	colnames(retval) = c("shape", "scale", "k", "value")
 
 	# We sort it by value
 	sortedIdx = sort.list(retval$value, decreasing=TRUE)
@@ -50,6 +47,6 @@ gengamma.lines = function(samples, params, ...){
 	maxVal = max(samples) * 1.1;
 
 	x = seq(minVal, maxVal, length=200)
-	y = dgengamma(x, mu=params[1], sigma=params[2], Q=params[3])
+	y = dgengamma.orig(x, shape=params[1], scale=params[2], k=params[3])
 	lines(x, y, ...)
 }
