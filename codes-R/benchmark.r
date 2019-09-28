d2f4a7c2544e1d2d3ad3aefff81dd679d66acfcf
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-09-28 00:14:08 -0300

    Improve benchmark

--- codes-R/benchmark.r
+++ codes-R/benchmark.r
@@ -1,7 +1,25 @@
+require(microbenchmark)
 require(elfDistr)
 source("kwcwg.r")
 
-time = system.time( kwcwg.pdf(seq(0.1, 2, length=5000000), 0.5, 1, 1, 1, 1) )
-cat("R Implementation: ", time["elapsed"], "\n")
-time = system.time( dkwcwg(seq(0.1, 2, length=5000000), 0.5, 1, 1, 1, 1) )
-cat("C++ Implementation: ", time["elapsed"], "\n")
+setOption("width", 150)
+
+x = seq(0.1, 2, length=50000)
+
+result = microbenchmark(
+	kwcwg.pdf(1, 0.5, 1, 1, 1, 1),
+	dkwcwg(1, 0.5, 1, 1, 1, 1),
+	unit="ms",
+	control=list(warmup=1000)
+)
+cat("\n\n============================\n")
+print(result)
+
+result = microbenchmark(
+	kwcwg.pdf(x, 0.5, 1, 1, 1, 1),
+	dkwcwg(x, 0.5, 1, 1, 1, 1),
+	unit="s",
+	control=list(warmup=10)
+)
+cat("\n\n============================\n")
+print(result)
