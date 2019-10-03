e5a715b7bd62109dfbb5af2fc09b2063aff1dbaf
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-10-03 00:26:34 -0300

    Add one more test

--- codes-R/rcpp-tests.r
+++ codes-R/rcpp-tests.r
@@ -38,6 +38,18 @@ h = function(x){
 	return(x);
 }
 
+# This is terrible.
+#i = function(x){
+#	return(apply(as.matrix(x), 1, function(elem){
+#			A = elem**2
+#			B = exp(A)
+#			C = sqrt(B)
+#			D = log(C)
+#			E = D**10
+#			return(log2(E))
+#		}))
+#}
+
 x = 1:1e5
 result = microbenchmark::microbenchmark(f(x), g(x), h(x), unit="ms")
 
