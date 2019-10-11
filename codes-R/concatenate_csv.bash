5b22d249926d935a70ef3d5279cfa6765519c553
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-10-11 19:29:32 -0300

    Add helpful bash script

---
+++ codes-R/concatenate_csv.bash
@@ -0,0 +1,22 @@
+allDirs="bfgs-estimate-c bfgs-subtract-min bfgs-subtract-estimated-min bfgs-no-subtract-c"
+
+for dir in $allDirs; do
+	ofile="$dir".csv
+	files="$(ls $dir/*.csv)"
+
+	arr=($files)
+	firstFile=${arr[0]}
+	cat $firstFile | head -1 | sed -e "s/^/\"directory\",/g" > $ofile
+
+	for i in $files; do
+		cat $i | tail +2 | sed -e "s/^/\"$dir\",/g";
+	done >> $ofile
+done
+
+arr=($allDirs)
+firstFile=${arr[0]}.csv
+cat $firstFile | head -1 > all.csv
+
+for dir in $allDirs; do
+	cat $dir.csv | tail +2
+done >> all.csv
