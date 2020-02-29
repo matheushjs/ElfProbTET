2fcc27f1636cca3581623864196284f47b69440f
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-02-29 03:17:43 -0300

    Begin min-estimators

---
+++ min-estimators/myoptim.r
@@ -0,0 +1,31 @@
+
+myoptim = function(params, f, ...){
+	result = try(optim(params, f, ...))
+
+	if(is.list(result))
+		return(result)
+	else
+		return(list(par=rep(0, length(params)), value=1e300, convergence=0))
+}
+
+cross.validation = function(params, samples, likelihood, useC=F, folds=5, ...){
+	foldLen = length(samples) / folds;
+	testResults = rep(0, folds);
+	samples = sample(samples);
+
+	for(i in 1:folds){
+		foldIdx = (foldLen*(i-1) + 1):(foldLen*i);
+		test = samples[foldIdx];
+		train = samples[-foldIdx];
+
+		if(useC == FALSE){
+			result = myoptim(params, function(p) likelihood(train, p, C=0), ...);
+			testResults[i] = likelihood(test, result$par, C=0);
+		} else {
+			result = myoptim(params, function(p) likelihood(train, p, C=p[length(p)]), ...);
+			testResults[i] = likelihood(test, result$par, C=result$par[length(result$par)]);
+		}
+	}
+
+	mean(testResults);
+}
