513752d6254c7280130132c82173cec24988cb4b
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-03-03 20:59:44 -0300

    Add an effective minimum

--- min-estimators/prog.r
+++ min-estimators/prog.r
@@ -6,21 +6,22 @@ estimator.log10 = function(data){
 }
 
 estimator.mcdiarmid = function(data){
-	sqrt(-log(0.01 / 2) / (2*n));
+	min(data) * (1 - sqrt(-log(0.01 / 2) / (2*length(data))));
 }
 
 gamma.test = function(estimator, sampleSize=100, numIter=100){
 	results = NULL;
 
-	for(shape in c(0.25, 0.5, 1, 2, 5, 10, 20)){
-	for(scale in c(0.25, 0.5, 1, 2, 5, 10, 20)){
+	for(shape in c(0.25, 0.5, 1, 2, 5, 10, 20, 40)){
+	for(scale in c(0.25, 0.5, 1, 2, 5, 10, 20, 40)){
 		squares = rep(0, numIter);
 		for(i in seq(numIter)){
 			realMin = runif(n=1, min=0, max=100);
 			data = realMin + rgamma(n=sampleSize, shape=shape, scale=scale);
-			estimatedMin = estimator.log10(data);
-			squares[i] = (realMin - estimatedMin)**2;
-			print(c(realMin, realMin, estimatedMin));
+			effectiveMinimum = realMin + qgamma(0.001, shape=shape, scale=scale);
+			estimatedMin = estimator(data);
+			squares[i] = (effectiveMinimum - estimatedMin)**2;
+			print(c(realMin, effectiveMinimum, estimatedMin));
 		}
 		results = rbind(results, c(shape, scale, sqrt(mean(squares))));
 		print(tail(results));
