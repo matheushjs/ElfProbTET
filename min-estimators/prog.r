491f82e9d5c53d3aa4a8e0567729f85d90d17678
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-03-28 21:29:25 -0300

    First experiments with gamma

--- min-estimators/prog.r
+++ min-estimators/prog.r
@@ -12,18 +12,41 @@ estimator.mcdiarmid = function(data){
 gamma.test = function(estimator, sampleSize=100, numIter=100){
 	results = NULL;
 
+	# The likelihood function
+	likelihood = function(ourSamples, params, C=0){
+		ourSamples = ourSamples - C
+
+		allLogs = dgamma(ourSamples, shape=params[1], scale=params[2], log=TRUE)
+
+		problems = which(!is.finite(allLogs))
+		allLogs[problems] = log(1e-300); # Merely to force optim to continue optimizing
+		if(length(problems) > 0 && length(problems) < 5){
+			warning(paste("gamma: Low amount (<5) of warnings at points:", ourSamples[problems]), call.=FALSE)
+		}
+
+		theSum = -sum(allLogs) # Invert so that minimization yields a maximum
+		
+		return(theSum)
+	}
+
 	for(shape in c(0.25, 0.5, 1, 2, 5, 10, 20, 40)){
 	for(scale in c(0.25, 0.5, 1, 2, 5, 10, 20, 40)){
-		squares = rep(0, numIter);
-		for(i in seq(numIter)){
-			realMin = runif(n=1, min=0, max=100);
-			data = realMin + rgamma(n=sampleSize, shape=shape, scale=scale);
-			effectiveMinimum = realMin + qgamma(0.001, shape=shape, scale=scale);
+		table = NULL;
+		for(i in 1:numIter){
+			data = rgamma(n=sampleSize, shape=shape, scale=scale);
 			estimatedMin = estimator(data);
-			squares[i] = (effectiveMinimum - estimatedMin)**2;
-			print(c(realMin, effectiveMinimum, estimatedMin));
+			subData = data - estimatedMin;
+
+			estimatedShape = shape
+			estimatedScale = mean(subData) / estimatedShape
+
+			result1 = myoptim(c(estimatedShape, estimatedScale), function(p) likelihood(subData, p), lower=c(0, 0), upper=c(Inf, Inf), method="L-BFGS-B")
+			result2 = myoptim(c(shape, scale), function(p) likelihood(data, p), lower=c(0, 0), upper=c(Inf, Inf), method="L-BFGS-B")
+
+			#print(c(estimatedMin, -result$value, result$par));
+			table = rbind(table, c(estimatedMin, -result1$value, -result2$value));
 		}
-		results = rbind(results, c(shape, scale, sqrt(mean(squares))));
+		results = rbind(results, colMeans(table));
 		print(tail(results));
 	}
 	}
