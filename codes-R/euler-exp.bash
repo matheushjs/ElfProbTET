da73a3dd0a6e62b860d1cb71f6817a2f8055a41a
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-06-03 00:02:31 -0300

    Add experiment

---
+++ codes-R/euler-exp.bash
@@ -0,0 +1,13 @@
+#PBS -N RInfer
+#PBS -l ncpus=8
+#PBS -l walltime=24:00:00
+#PBS -m abe
+#PBS -M matheus.saldanha@usp.br
+
+TIMEFORMAT="%E";
+
+# load modules
+module load gcc/4.9.2;
+module load R/3.5.0;
+
+R -f inference.r;
